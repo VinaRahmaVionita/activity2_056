f40e8aae8a80e181252d9622b4a9a671c915781d
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-03-08 12:07:27 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){ //begin
+
+//Numeric Luas, P, L
+double Luas, P, L;
+//Display "masukkan panjangnya = "
+cout << "masukkan panjangnya = ";
+//Accept P
+cin >> P;
+//Display "masukkan lebarnya = "
+cout << "masukkan lebarnya = ";
+//Accept L
+cin >> L;
+// Compute Luas = P*L
+Luas = P*L;
+//Display "Luasnya = " + Luas
+cout << "Luasnya = " << Luas;
+}//End
